495217f326ce97f62a81c0673b620ea49306d405
SHIRLY JUSID <46875521@ort>
2023-06-01 10:17:08 -0300

    UseState

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,34 @@
 
 import './App.css';
-import Formulario from './Formulario.js';
-import Citas from './Citas.js';
-import ListadoCitas from './ListadoCitas.js';
+import React, { useState } from "react";
+import Formulario from './Componentes/Formulario';
+import Citas from './Componentes/Citas';
+import ListadoCitas from './Componentes/ListadoCitas';
 
 function App() {
   const listado = [
     {Mascota:'Nina', Dueño:'Martin', Fecha:'2023-05-02',Hora:23-20, Sintomas:'toz'}
   ];
+  const [citas, setCitas] = useState([{
+    id: 0,
+    nomMascota: "prueba",
+    nomDueno: "prueba",
+    fecha: "prueba",
+    hora: "prueba",
+    sintomas: "prueba"
+  }]);
+
+  const AgregarCita = (newCita) => {
+
+
+    setCitas(
+      [
+        ...citas,
+        newCita,
+      ]
+    );
+  }
+
   return (
     <div id="root">
       <h1>ADMINISTRADOR DE PACIENTES</h1>
--- my-react-app/src/Componentes/Formulario.js
+++ my-react-app/src/Componentes/Formulario.js
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function Formulario() {
-    
-   
+
+export default function Formulario({ onAgregarCita }) {
+
+    const [idState, setId] = useState(0);
+
+    const CrearCita = (e) => {
+
+        e.preventDefault();
+
+        let newCita = {
+
+            id: idState,
+            nomMascota: e.target.mascota.value,
+            nomDueno: e.target.propietario.value,
+            fecha: e.target.fecha.value,
+            hora: e.target.hora.value,
+            sintomas: e.target.sintomas.value
+        }
+
+        setId(idState + 1)
+
+        e.target.mascota.value = ''
+        e.target.propietario.value = ''
+        e.target.fecha.value = ''
+        e.target.hora.value = ''
+        e.target.sintomas.value = ''
+
+        onAgregarCita(newCita);
+    }
+
     return (
-        <>
+        <form onSubmit={(e) => crearCita(e)}>
             <label>Nombre Mascota</label><input type="text" name="mascota" className="u-full-width" placeholder="Nombre Mascota" />
             <label>Nombre Dueño</label>
             <input type="text" name="propietario" className="u-full-width" placeholder="Nombre dueño de la mascota" id="nombreM"/>
@@ -13,6 +40,6 @@ export default function Formulario() {
             <input type="time" name="hora" className="u-full-width" /><label>Sintomas</label>
             <textarea name="sintomas" className="u-full-width"></textarea>
             <button type="submit" className="u-full-width button-primary" >Agregar Cita</button>
-        </>
+        </form>
     )
 }
